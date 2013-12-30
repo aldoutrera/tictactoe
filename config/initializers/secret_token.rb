57b6dfc5320257b03f61613ffe60d968ce9275c5
Aldo Utrera <Kugel85@gmail.com>
2013-12-30 00:20:37 -0600

    Init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Platanus::Application.config.secret_key_base = '4715669fe013ae4291d2b59852f44d3b64aa8cd474deeb9694fda24fa1ae22b7570b001a48ee2347600015deb547548c5fe4168ec5c38f942d43df3d7a896ca9'
