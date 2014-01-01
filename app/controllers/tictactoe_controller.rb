250b92dbc858a27eb3bfa4ac7d74cf9066b4114a
Aldo Utrera <Kugel85@gmail.com>
2014-01-01 17:20:24 -0600

    Se agregó la lógica para crear un juego nuevo y la llamada de la api para hacer un movimiento del juego

--- app/controllers/tictactoe_controller.rb
+++ app/controllers/tictactoe_controller.rb
@@ -1,4 +1,31 @@
 class TictactoeController < ApplicationController
   def new
+    clear_session
+    @tictactoe = Tictactoe.new
+    @tictactoe.start
+    update_session
+    respond_to do |format|
+      format.html { render json: @tictactoe }
+    end
+  end
+
+  def move
+    @tictactoe = Tictactoe.new
+    @tictactoe.board = session[:game].board
+    @tictactoe.player_turn(params[:number].to_i)
+    update_session
+    respond_to do |format|
+      format.html { render json: @tictactoe }
+    end
+  end
+
+  private
+  def update_session
+    session[:game] = @tictactoe
+  end
+
+  private
+  def clear_session
+    session[:game] = nil
   end
 end
