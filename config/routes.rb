25bccb102febacad4c93149d79bdd12d3909ff8f
Aldo Utrera <Kugel85@gmail.com>
2014-01-01 17:21:31 -0600

    Agregando las rutas para el index de la app, y 2 llamadas de la api, para crear un juego nuevo y para hacer un movimiento

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Platanus::Application.routes.draw do
-  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'welcome/index'
+  get 'tictactoe/new(.:format)'
+  get 'tictactoe/move/:number(.:format)' => 'tictactoe#move'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
